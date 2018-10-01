16764a0c0c9fe4efab9ab3a8ddf474cbda8a08c5
Samir Mammadli <mammadli.s86@gmail.com>
2018-10-01 17:36:22 +0400

    1

---
+++ CreationalBuilder/CreationalBuilder/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DotNetDesignPatternDemos.Creational.Builder
+{
+    class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indent * indentSize);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (string.IsNullOrWhiteSpace(Text))
+            {
+                sb.AppendLine(new string(' ', (indent + 1) * indentSize));
+                sb.Append(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+
+            sb.AppendLine
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0); 
+        }
+
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            // if you want to build a simple HTML paragraph using StringBuilder
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            WriteLine(sb);
+
+            // now I want an HTML list with 2 words in it
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            WriteLine(sb);
+
+            // ordinary non-fluent builder
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            WriteLine(builder.ToString());
+
+            // fluent builder
+            sb.Clear();
+            builder.Clear(); // disengage builder from the object it's building, then...
+            builder.AddChildFluent("li", "hello").AddChildFluent("li", "world");
+            WriteLine(builder);
+        }
+    }
+}
